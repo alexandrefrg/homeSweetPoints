6ecb3e1b022bfa25174a6797fe4a6cca191ff7dd
Alexandre <alexandrefrg@hotmail.com>
2020-03-26 19:41:50 -0300

    fix fk not working 'syntax error' and generated new DB

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -11,6 +11,9 @@ module.exports = {
       directory: './src/database/migrations'
     },
     useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)
+    },
   },
 
   staging: {
