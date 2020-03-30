4df75a54a68b4fdb6444661725a84b7ec8fe6c4d
Alexandre <alexandrefrg@hotmail.com>
2020-03-30 15:51:21 -0300

    creating the project structure for back-end

---
+++ backend/src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
+const routes = express.Router();
+
+//here go the back-end routes();
+
+module.exports = routes
