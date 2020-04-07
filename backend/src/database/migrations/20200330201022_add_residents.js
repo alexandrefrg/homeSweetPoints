f343eb673722232fd495842f354ea84f777e2ad1
Alexandre <alexandrefrg@hotmail.com>
2020-04-07 00:23:58 -0300

    refactoring house and residents admin dependecy

--- backend/src/database/migrations/20200330201022_add_residents.js
+++ backend/src/database/migrations/20200330201022_add_residents.js
@@ -1,35 +1,22 @@
 const generateUniqueToken = require("../../utils/generateUniqueId");
 
 exports.up = function (knex) {
-  return Promise.all([
-    knex.schema
-      .createTable("residents", function (table) {
-        table.string("uniqueToken").primary();
+  return knex.schema.createTable("residents", function (table) {
+    table.string("uniqueToken").primary();
 
-        table.string("email").notNullable().unique();
-        table.string("name").notNullable();
-        table.decimal("totalPoints").notNullable().defaultTo(0);
+    table.string("email").notNullable().unique();
+    table.string("name").notNullable();
+    table.decimal("totalPoints").notNullable().defaultTo(0);
 
-        table.string("role").notNullable().defaultTo("HouseResident");
-        table.foreign("role").references("title").inTable("roles");
+    table.string("role").notNullable().defaultTo("HouseResident");
+    table.foreign("role").references("title").inTable("roles");
 
-        table.string("house_uniqueToken").notNullable();
-        table
-          .foreign("house_uniqueToken")
-          .references("uniqueToken")
-          .inTable("house");
-      })
-      .then(function () {
-        return knex("residents").insert({
-          uniqueToken: generateUniqueToken(),
-          email: "admin@admin.com",
-          name: "admin",
-          totalPoints: 0,
-          role: "HouseAdmin",
-          house_uniqueToken: "",
-        });
-      }),
-  ]);
+    table.string("house_uniqueToken").notNullable();
+    table
+      .foreign("house_uniqueToken")
+      .references("uniqueToken")
+      .inTable("house");
+  });
 };
 
 exports.down = function (knex) {
